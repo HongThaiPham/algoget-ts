18d7042e96d011c75c4ec88378e725e8e5931e3e
leopham <hongthaipro@gmail.com>
2024-01-02 10:33:24 +0700

    clone from py

--- __test__/algobet-ts.test.ts
+++ __test__/algobet-ts.test.ts
@@ -1,41 +1,176 @@
-import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { describe, test, beforeAll, beforeEach, expect } from '@jest/globals';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import * as algokit from '@algorandfoundation/algokit-utils';
+import algosdk, { Account, encodeAddress, makeBasicAccountTransactionSigner } from 'algosdk';
+import { algos, getOrCreateKmdWalletAccount, microAlgos } from '@algorandfoundation/algokit-utils';
 import { AlgobetTsClient } from '../contracts/clients/AlgobetTsClient';
 
 const fixture = algorandFixture();
 
-let appClient: AlgobetTsClient;
-
 describe('AlgobetTs', () => {
+  let algod: algosdk.Algodv2;
+
+  let appClient: AlgobetTsClient;
+  let oracleAccount: Account;
+  let managerAccount: Account;
+  let sender: Account;
+  let appId: bigint | number;
+  let appAddress: string;
+  const eventStartUnixTimestamp = Math.floor(new Date().getTime() / 1000) + 10;
+  const eventEndUnixTimestamp = eventStartUnixTimestamp + 2000;
+
   beforeEach(fixture.beforeEach);
 
   beforeAll(async () => {
     await fixture.beforeEach();
-    const { algod, testAccount } = fixture.context;
+    const { testAccount, kmd } = fixture.context;
+    managerAccount = testAccount;
+    algod = fixture.context.algod;
+
+    oracleAccount = await getOrCreateKmdWalletAccount(
+      {
+        name: 'unencrypted-default-wallet',
+        fundWith: algos(10_000_000),
+      },
+      algod,
+      kmd
+    );
+
+    sender = await getOrCreateKmdWalletAccount(
+      {
+        name: 'tealscript-test-wallet',
+        fundWith: algos(10),
+      },
+      algod,
+      kmd
+    );
 
     appClient = new AlgobetTsClient(
       {
-        sender: testAccount,
+        sender: managerAccount,
         resolveBy: 'id',
         id: 0,
       },
       algod
     );
 
-    await appClient.create.createApplication({});
+    await appClient.create.createApplication({
+      managerAddr: managerAccount.addr,
+      oracleAddr: oracleAccount.addr,
+      eventStartUnixTimestamp: BigInt(eventStartUnixTimestamp),
+      eventEndUnixTimestamp: BigInt(eventEndUnixTimestamp),
+      payoutTimeWindowS: 5,
+    });
+
+    const appRef = await appClient.appClient.getAppReference();
+    appId = appRef.appId;
+    appAddress = appRef.appAddress;
+
+    const atc = new algosdk.AtomicTransactionComposer();
+    const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from: sender.addr,
+      to: appAddress,
+      amount: 100_000,
+      suggestedParams: await algod.getTransactionParams().do(),
+    });
+    const signer = makeBasicAccountTransactionSigner(sender);
+    atc.addTransaction({ txn, signer });
+    await atc.execute(algod, 3);
   });
 
-  test('sum', async () => {
-    const a = 13;
-    const b = 37;
-    const sum = await appClient.doMath({ a, b, operation: 'sum' });
-    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  test('Init', async () => {
+    const globalState = await appClient.getGlobalState();
+    const managerType = algosdk.ABIType.from('address');
+    expect(managerType.decode(globalState.manager!.asByteArray())).toEqual(managerAccount.addr);
+    expect(encodeAddress(globalState.oracleAddr!.asByteArray())).toEqual(oracleAccount.addr);
   });
 
-  test('difference', async () => {
-    const a = 13;
-    const b = 37;
-    const diff = await appClient.doMath({ a, b, operation: 'difference' });
-    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  test('Optin application', async () => {
+    await appClient.optIn.optInToApplication(
+      {},
+      {
+        sender,
+      }
+    );
+    const localState = await appClient.getLocalState(sender.addr);
+    const valueType = algosdk.ABIType.from('bool');
+    expect(valueType.decode(localState.hasPlacedBet!.asByteArray())).toEqual(false);
+    expect(valueType.decode(localState.hasRequestedPayout!.asByteArray())).toEqual(false);
+  });
+
+  test('Bet', async () => {
+    let globalState = await appClient.getGlobalState();
+    const betAmount = globalState.betAmount!.asNumber();
+    const paymentTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from: sender.addr,
+      to: appAddress,
+      amount: betAmount,
+      suggestedParams: await algokit.getTransactionParams(undefined, algod),
+    });
+
+    await appClient.bet(
+      {
+        opt: 0,
+        betDepositTx: paymentTxn,
+      },
+      {
+        sender,
+      }
+    );
+    globalState = await appClient.getGlobalState();
+    expect(globalState.counterOpt0!.asNumber()).toEqual(1);
+    expect(globalState.stakeAmount!.asNumber()).toEqual(betAmount);
+    const localState = await appClient.getLocalState(sender.addr);
+    const valueType = algosdk.ABIType.from('bool');
+    expect(valueType.decode(localState.hasPlacedBet!.asByteArray())).toEqual(true);
+    expect(valueType.decode(localState.hasRequestedPayout!.asByteArray())).toEqual(false);
+    expect(localState.chosenOpt!.asNumber()).toEqual(0);
+  });
+
+  test('Set event (Negative)', async () => {
+    await expect(appClient.setEventResult({ opt: 0 })).rejects.toThrow();
+  });
+
+  test('Set event', async () => {
+    await appClient.setEventResult(
+      { opt: 0 },
+      {
+        sender: oracleAccount,
+      }
+    );
+    const globalState = await appClient.getGlobalState();
+    expect(globalState.eventResult!.asNumber()).toEqual(0);
+    expect(globalState.winningPayout!.asNumber()).toBeGreaterThan(0);
+  });
+
+  test('Payout', async () => {
+    await appClient.payout(
+      {},
+      {
+        sender,
+        sendParams: {
+          fee: microAlgos(2000),
+        },
+      }
+    );
+    const localState = await appClient.getLocalState(sender.addr);
+    const valueType = algosdk.ABIType.from('bool');
+    expect(valueType.decode(localState.hasPlacedBet!.asByteArray())).toEqual(true);
+    expect(valueType.decode(localState.hasRequestedPayout!.asByteArray())).toEqual(true);
+  }, 10000);
+
+  test.skip('Delete application', async () => {
+    await appClient.delete.deleteApplication(
+      {},
+      {
+        sendParams: {
+          fee: microAlgos(2000),
+        },
+      }
+    );
+    await expect(appClient.getGlobalState()).rejects.toThrow();
   });
 });
+function AccountTransactionSigner(sk: any): any {
+  throw new Error('Function not implemented.');
+}
--- contracts/algobet-ts.algo.ts
+++ contracts/algobet-ts.algo.ts
@@ -2,46 +2,154 @@ import { Contract } from '@algorandfoundation/tealscript';
 
 // eslint-disable-next-line no-unused-vars
 class AlgobetTs extends Contract {
-  /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: number, b: number): number {
-    return a + b;
+  manager = GlobalStateKey<Address>();
+
+  oracleAddr = GlobalStateKey<Address>();
+
+  eventResult = GlobalStateKey<uint64>();
+
+  betAmount = GlobalStateKey<uint64>();
+
+  counterOpt0 = GlobalStateKey<uint64>();
+
+  counterOpt1 = GlobalStateKey<uint64>();
+
+  counterOpt2 = GlobalStateKey<uint64>();
+
+  stakeAmount = GlobalStateKey<uint64>();
+
+  winningCount = GlobalStateKey<uint64>();
+
+  winningPayout = GlobalStateKey<uint64>();
+
+  eventStartTimestamp = GlobalStateKey<uint64>();
+
+  eventEndTimestamp = GlobalStateKey<uint64>();
+
+  payoutTimeWindowS = GlobalStateKey<uint64>();
+
+  chosenOpt = LocalStateKey<uint64>();
+
+  hasPlacedBet = LocalStateKey<boolean>();
+
+  hasRequestedPayout = LocalStateKey<boolean>();
+
+  createApplication(
+    managerAddr: Address,
+    oracleAddr: Address,
+    eventStartUnixTimestamp: uint64,
+    eventEndUnixTimestamp: uint64,
+    payoutTimeWindowS: uint64
+  ): void {
+    this.manager.value = managerAddr;
+    this.oracleAddr.value = oracleAddr;
+
+    assert(eventEndUnixTimestamp > globals.latestTimestamp);
+    assert(eventEndUnixTimestamp > eventStartUnixTimestamp);
+    this.eventResult.value = 99;
+    this.betAmount.value = 1_400_000;
+    this.stakeAmount.value = 0;
+    this.counterOpt0.value = 0;
+    this.counterOpt1.value = 0;
+    this.counterOpt2.value = 0;
+    this.winningCount.value = 0;
+    this.eventStartTimestamp.value = eventStartUnixTimestamp;
+    this.eventEndTimestamp.value = eventEndUnixTimestamp;
+    this.payoutTimeWindowS.value = payoutTimeWindowS;
   }
 
-  /**
-   * Calculates the difference between two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
-   */
-  private getDifference(a: number, b: number): number {
-    return a >= b ? a - b : b - a;
+  set_event_result(opt: uint64): void {
+    assert(this.txn.sender === this.oracleAddr.value);
+    // assert(globals.latestTimestamp >= this.eventEndTimestamp.value);
+    if (opt > 3) {
+      throw Error('Invalid option');
+    }
+    this.eventResult.value = opt;
+    if (opt === 0) {
+      this.winningCount.value = this.counterOpt0.value;
+    }
+    if (opt === 1) {
+      this.winningCount.value = this.counterOpt1.value;
+    }
+    if (opt === 2) {
+      this.winningCount.value = this.counterOpt2.value;
+    }
+    if (this.winningCount.value === 0) {
+      this.winningPayout.value = this.stakeAmount.value - globals.minTxnFee;
+    } else {
+      this.winningPayout.value = this.stakeAmount.value / this.winningCount.value - globals.minTxnFee;
+    }
   }
 
-  /**
-   * A method that takes two numbers and does either addition or subtraction
-   *
-   * @param a The first number
-   * @param b The second number
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
-   */
-  doMath(a: number, b: number, operation: string): number {
-    let result: number;
+  deleteApplication(): void {
+    assert(this.txn.sender === this.manager.value);
+    // assert(globals.latestTimestamp >= this.eventEndTimestamp.value);
+    // assert(globals.latestTimestamp >= this.eventEndTimestamp.value + this.payoutTimeWindowS.value);
+    sendPayment({
+      receiver: this.txn.sender,
+      amount: 0,
+      closeRemainderTo: this.txn.sender,
+    });
+  }
 
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
+  private setManager(newManager: Address): void {
+    this.manager.value = newManager;
+  }
 
-    return result;
+  private setOracle(newOracle: Address): void {
+    this.oracleAddr.value = newOracle;
+  }
+
+  private setEventStartTimestamp(newEventStartTimestamp: uint64): void {
+    this.eventStartTimestamp.value = newEventStartTimestamp;
+  }
+
+  private setEventEndTimestamp(newEventEndTimestamp: uint64): void {
+    this.eventEndTimestamp.value = newEventEndTimestamp;
+  }
+
+  private setPayoutTimeWindowS(newPayoutTimeWindowS: uint64): void {
+    this.payoutTimeWindowS.value = newPayoutTimeWindowS;
+  }
+
+  optInToApplication(): void {
+    this.hasPlacedBet(this.txn.sender).value = false;
+    this.hasRequestedPayout(this.txn.sender).value = false;
+  }
+
+  bet(opt: uint64, betDepositTx: PayTxn): void {
+    verifyPayTxn(betDepositTx, {
+      amount: this.betAmount.value,
+      receiver: this.app.address,
+    });
+    // assert(globals.latestTimestamp < this.eventStartTimestamp.value);
+
+    assert(this.hasPlacedBet(this.txn.sender).value === false);
+    if (opt > 3) {
+      throw Error('Invalid option');
+    }
+    this.chosenOpt(this.txn.sender).value = opt;
+    if (opt === 0) {
+      this.counterOpt0.value = this.counterOpt0.value + 1;
+    }
+    if (opt === 1) {
+      this.counterOpt1.value = this.counterOpt1.value + 1;
+    }
+    if (opt === 2) {
+      this.counterOpt2.value = this.counterOpt2.value + 1;
+    }
+    this.hasPlacedBet(this.txn.sender).value = true;
+    this.stakeAmount.value = this.stakeAmount.value + this.betAmount.value;
+  }
+
+  payout(): void {
+    assert(this.txn.sender.isOptedInToApp(this.app));
+    assert(this.eventResult.value === this.chosenOpt(this.txn.sender).value);
+    assert(this.hasRequestedPayout(this.txn.sender).value === false);
+    this.hasRequestedPayout(this.txn.sender).value = true;
+    sendPayment({
+      receiver: this.txn.sender,
+      amount: this.winningPayout.value,
+    });
   }
 }
